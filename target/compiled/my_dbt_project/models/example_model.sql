e44417c0a77dde122c6dcdb1c87cd46f5caa57fe
fynitecorp <tahir.warraich@fynite.ai>
2024-05-21 15:41:53 -0500

    Add files via upload

---
+++ target/compiled/my_dbt_project/models/example_model.sql
@@ -0,0 +1,16 @@
+-- models/example_model.sql
+
+WITH source_data AS (
+    SELECT
+        column1,
+        column2,
+        column3
+    FROM DB_FOR_DBT.FIVETRAN_METADATA.EXAMPLE_DATA
+)
+
+SELECT
+    column1,
+    column2,
+    column3,
+    column1 + column2 + column3 AS column4
+FROM source_data;
